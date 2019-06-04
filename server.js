21363f860927b2a838069a8f7d842c1560e2ed62
Inti <intiv96@gmail.com>
2019-06-04 15:19:58 -0600

    Adding packages to prepare to add react, set express headers

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+
+
 app.listen(port, (req, res) => {
     console.log(`App listening on port ${port}`);
-})
+});

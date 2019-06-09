a26bfdbfce0a5d9f6e6db49b80a75fbcd667d878
Inti <intiv96@gmail.com>
2019-06-09 16:31:14 -0600

    Setup to deploy to heroku

--- app/app.js
+++ app/app.js
@@ -23,9 +23,16 @@ app.use(express.json());
 
 //Routes
 app.use('/api/categories', require('../routes/categories'));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/public/index.html'));
-});
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}else{
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/public/index.html'));
+    });
+}
 
 module.exports = app;

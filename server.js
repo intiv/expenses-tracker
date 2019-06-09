9e08d008c082a7cc9732272fea4b8f689bed4159
Inti <intiv96@gmail.com>
2019-06-09 17:21:40 -0600

    Requires in server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require('./app/app');
 const port = process.env.PORT || 5000;
+const path = require('path');
+const express = require('express');
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + 'client/build/index.html'));

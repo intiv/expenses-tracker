d69cae40578d61c759aebcb87c3ba33e445c1607
Inti <intiv96@gmail.com>
2019-06-09 16:38:50 -0600

    Add engines, change file serving

--- app/app.js
+++ app/app.js
@@ -29,10 +29,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}else{
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/public/index.html'));
-    });
 }
 
 module.exports = app;

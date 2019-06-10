8056ab82e534283289449e9a44ad628ddc0ab5c7
Inti <intiv96@gmail.com>
2019-06-09 18:08:39 -0600

    Trying Switch on router for rendering

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Switch } from 'react-router-dom';
 import Categories from '../components/categories/categories';
 import Home from '../components/home/home';
 
 export const router = (
     <Router>
-        <Route exact path="/" component={Home}/>
-        <Route path="/api/categories" component={Categories}/>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/api/categories" component={Categories}/>
+        </Switch>
 
     </Router>
 )
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const port = process.env.PORT || 5000;
 const path = require('path');
 const express = require('express');
 
-if(process.env.NODE_ENV === 'production'){
+// if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+// }
 
 app.listen(port, (req, res) => {
     console.log(`App listening on port ${port}`);
